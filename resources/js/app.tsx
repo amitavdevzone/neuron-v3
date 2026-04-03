a21c6dfcbeadf38af3ef6f3e303db7fcb61f8026
Amitav Roy <reachme@amitavroy.com>
2026-04-03 20:18:29 +0530

    feat: upgraded inertia to v3

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,20 +1,16 @@
 import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import '../css/app.css';
 import { initializeTheme } from '@/hooks/use-appearance';
+import { resolveInertiaPage } from '@/lib/resolve-inertia-page';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.tsx`,
-            import.meta.glob('./pages/**/*.tsx'),
-        ),
+    resolve: (name) => resolveInertiaPage(name),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
